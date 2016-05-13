b8b12b4159391a39d2e8d17f040ad84a59899f3b
tchangla <rulcume@gmail.com>
2016-05-13 18:45:37 +0200

    Cambios Raúl
    He añadido que cuando haces clic en una fila del "DataGridView", te
    carga un video en la vista del video.

--- Propuestas.Designer.cs
+++ Propuestas.Designer.cs
@@ -32,20 +32,21 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Propuestas));
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.vIRALIZEDataSet = new ViralizeDesktop.VIRALIZEDataSet();
-            this.pROPUESTARETOBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.pROPUESTA_RETOTableAdapter = new ViralizeDesktop.VIRALIZEDataSetTableAdapters.PROPUESTA_RETOTableAdapter();
             this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.tituloDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.descripcionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.fechaPublicacionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.urlVideoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.usuarioIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pROPUESTARETOBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.vIRALIZEDataSet = new ViralizeDesktop.VIRALIZEDataSet();
+            this.pROPUESTA_RETOTableAdapter = new ViralizeDesktop.VIRALIZEDataSetTableAdapters.PROPUESTA_RETOTableAdapter();
             this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.vIRALIZEDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pROPUESTARETOBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vIRALIZEDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -63,6 +64,8 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.AutoGenerateColumns = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
@@ -75,22 +78,11 @@
             this.dataGridView1.DataSource = this.pROPUESTARETOBindingSource;
             this.dataGridView1.Location = new System.Drawing.Point(29, 60);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(484, 253);
             this.dataGridView1.TabIndex = 1;
-            // 
-            // vIRALIZEDataSet
-            // 
-            this.vIRALIZEDataSet.DataSetName = "VIRALIZEDataSet";
-            this.vIRALIZEDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // pROPUESTARETOBindingSource
-            // 
-            this.pROPUESTARETOBindingSource.DataMember = "PROPUESTA_RETO";
-            this.pROPUESTARETOBindingSource.DataSource = this.vIRALIZEDataSet;
-            // 
-            // pROPUESTA_RETOTableAdapter
-            // 
-            this.pROPUESTA_RETOTableAdapter.ClearBeforeFill = true;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
             // 
             // idDataGridViewTextBoxColumn
             // 
@@ -128,6 +120,20 @@
             this.usuarioIDDataGridViewTextBoxColumn.HeaderText = "usuarioID";
             this.usuarioIDDataGridViewTextBoxColumn.Name = "usuarioIDDataGridViewTextBoxColumn";
             // 
+            // pROPUESTARETOBindingSource
+            // 
+            this.pROPUESTARETOBindingSource.DataMember = "PROPUESTA_RETO";
+            this.pROPUESTARETOBindingSource.DataSource = this.vIRALIZEDataSet;
+            // 
+            // vIRALIZEDataSet
+            // 
+            this.vIRALIZEDataSet.DataSetName = "VIRALIZEDataSet";
+            this.vIRALIZEDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // pROPUESTA_RETOTableAdapter
+            // 
+            this.pROPUESTA_RETOTableAdapter.ClearBeforeFill = true;
+            // 
             // axWindowsMediaPlayer1
             // 
             this.axWindowsMediaPlayer1.Enabled = true;
@@ -148,6 +154,15 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(420, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Propuestas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -155,6 +170,7 @@
             this.BackgroundImage = global::ViralizeDesktop.Properties.Resources.colorBackground;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(909, 545);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.axWindowsMediaPlayer1);
             this.Controls.Add(this.dataGridView1);
@@ -163,8 +179,8 @@
             this.Text = "Propuestas";
             this.Load += new System.EventHandler(this.Propuestas_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.vIRALIZEDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pROPUESTARETOBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vIRALIZEDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -186,5 +202,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn usuarioIDDataGridViewTextBoxColumn;
         private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- Propuestas.cs
+++ Propuestas.cs
@@ -16,6 +16,8 @@ namespace ViralizeDesktop
 
     public partial class Propuestas : Form
     {
+        string getVideoUrl = "";
+
         public Propuestas()
         {
             InitializeComponent();
@@ -26,9 +28,14 @@ namespace ViralizeDesktop
             // TODO: This line of code loads data into the 'vIRALIZEDataSet.PROPUESTA_RETO' table. You can move, or remove it, as needed.
             this.pROPUESTA_RETOTableAdapter.Fill(this.vIRALIZEDataSet.PROPUESTA_RETO);
 
-            axWindowsMediaPlayer1.URL = @"http://vreality.es/vidLow.mp4";
+            axWindowsMediaPlayer1.URL = @"";
             dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
             dataGridView1.MultiSelect = false;
+        }
+
+        private void CargarVideo(String videoUrl)
+        {
+            axWindowsMediaPlayer1.URL = @""+getVideoUrl;
         }
 
         private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
@@ -39,6 +46,26 @@ namespace ViralizeDesktop
         private void button1_Click(object sender, EventArgs e)
         {
             axWindowsMediaPlayer1.URL = @"http://vreality.es/dani.mp4";
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            //GridViewRow row = dataGridView1.SelectedRows;
+            Int32 selectedRowCount =
+                dataGridView1.Rows.GetRowCount(DataGridViewElementStates.Selected);
+            if (selectedRowCount > 0)
+            {
+                getVideoUrl = dataGridView1.SelectedRows[0].Cells[4].Value.ToString();
+                CargarVideo(getVideoUrl);
+                //MessageBox.Show(getVideoUrl);
+            }
         }
     }
 }
