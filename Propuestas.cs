a6b19f0cdea0fc2768c42e49b44bd79407832a71
Daniel Almansa Lopez <daniel.almansa93@gmail.com>
2016-05-19 14:55:50 +0200

    Boton estadisticas
    Boton estadisticas lleva al form correspondiente

--- Propuestas.cs
+++ Propuestas.cs
@@ -245,7 +245,8 @@ namespace ViralizeDesktop
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            Estadisticas est = new Estadisticas();
+            est.ShowDialog();
         }
     } 
 }
